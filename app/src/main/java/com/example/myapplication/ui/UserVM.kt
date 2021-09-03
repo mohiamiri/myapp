79e3c74e0a91b45a84903681fe41eb49b737047b
mohiamiri <mohammadamiri.py@gmail.com>
2021-09-03 03:19:53 +0200

    database operations added

--- app/src/main/java/com/example/myapplication/ui/UserVM.kt
+++ app/src/main/java/com/example/myapplication/ui/UserVM.kt
@@ -3,12 +3,17 @@ package com.example.myapplication.ui
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.myapplication.RemoteApi.retrofitService
+import com.example.myapplication.database.daos.UserDao
 import com.example.myapplication.database.entities.User
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
-class UserVM : ViewModel() {
+class UserVM(
+    private val userDao: UserDao
+) : ViewModel() {
     private val _strings = MutableStateFlow(emptyList<User>())
     val strings: StateFlow<List<User>> = _strings
 
@@ -19,11 +24,16 @@ class UserVM : ViewModel() {
     }
 
     fun save() {
-
+        viewModelScope.launch {
+            _strings.value.forEach {
+                userDao.addUser(it)
+            }
+        }
     }
 
     fun read() {
-
+        _strings.value = userDao.getAllUsers()
+            .stateIn(viewModelScope, SharingStarted.Eagerly, emptyList()).value
     }
 
     fun reset() {
